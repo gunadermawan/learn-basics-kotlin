933e99a5405e84bdc3b6df3dcadee9653312758c
itselaras <gunadermawan62@gmail.com>
2022-10-01 23:30:59 +0700

    create class inheritance and visibility

--- src/main/kotlin/com/gunder/kotlin/oop/Class.kt
+++ src/main/kotlin/com/gunder/kotlin/oop/Class.kt
@@ -1,16 +1,7 @@
 package com.gunder.kotlin.oop
 
-class Animal(val name: String, val age: Int){
-    fun eat(){
-        println("$name sedang makan!")
-    }
-    fun age(){
-        println("usianya adalah $age tahun")
-    }
-}
 
-fun main() {
-    val cat = Animal("kucing miaw", 2)
-    cat.eat()
-    cat.age()
-}
+
+
+
+
---
+++ src/main/kotlin/com/gunder/kotlin/oop/Visibility.kt
@@ -0,0 +1,10 @@
+package com.gunder.kotlin.oop
+
+
+internal class Animal( var name:String, var age:Int){
+}
+
+fun main() {
+    var cat = Animal("cemplin",2)
+    println(cat.name)
+}
